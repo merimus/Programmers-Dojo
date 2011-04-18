6de78f86de220c7c58a886abfd33a3fdad364417
Brian Makin <merimus@gmail.com>
2011-04-18 10:14:51 -0400

    cleanup

--- prime-generator/trial-division.c
+++ prime-generator/trial-division.c
@@ -1,8 +1,14 @@
 
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+
+#define THOUSAND 1000
+#define MILLION 1000000
 
 struct prime_elem_s {
   uint64_t prime;
@@ -25,6 +31,7 @@ void push_prime(prime_list_t *prime_list, uint64_t prime)
   if( prime_list->tail )
     {
       prime_list->tail->next = elem;
+      prime_list->tail = elem;
     } 
   else 
     {
@@ -37,7 +44,7 @@ int test_prime(prime_list_t *prime_list, uint64_t num)
 {
   prime_elem_t *elem = prime_list->head;
   uint64_t s = sqrt(num);
-  while( elem && elem->prime <= s )
+  while( elem->prime <= s )
     {
       if( num % elem->prime == 0 )
 	{
@@ -51,21 +58,33 @@ int test_prime(prime_list_t *prime_list, uint64_t num)
 int main(int argc, char *argv[])
 {
   prime_list_t *prime_list;
+  struct timeval start;
+  struct rusage ru;
+  struct timeval tv;
+  struct timeval elapsed;
+
+  gettimeofday(&start, NULL);
+
   prime_list = calloc(1, sizeof(prime_list_t));
   push_prime(prime_list, 2);
+  push_prime(prime_list, 3);
 
-  uint64_t n = 3;
+  uint64_t num = 4;
   while(1)
     {
-      if( test_prime(prime_list, n) )
+      if( test_prime(prime_list, num) )
 	{
-	  push_prime(prime_list, n);
-	  if( prime_list->length % (10*1000*1000) == 0 )
-	    printf("num_primes: %llu\n", prime_list->length);
-	  if( prime_list->length == 100*1000*1000 )
-	    return 1;
+	  push_prime(prime_list, num);
+
+	  if( prime_list->length % (100*THOUSAND) == 0 )
+	    {
+	      getrusage(RUSAGE_SELF, &ru);
+	      gettimeofday(&tv, NULL);
+	      timersub(&tv, &start, &elapsed);
+	      printf("num_primes: %.2f M Primes : %.2fMB : %.2f\n", prime_list->length / (double)MILLION, ru.ru_maxrss / 1024.0, elapsed.tv_sec+elapsed.tv_usec/(double)MILLION);
+	    }
 	}
-      n++;
+      num++;
     }
 
   return 0;
